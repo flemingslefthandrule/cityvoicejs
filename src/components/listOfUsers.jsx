cd207eb3c8a742d376a6382c03193e1fa129e6c4
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-03-12 21:12:54 +0530

    search

--- src/components/listOfUsers.jsx
+++ src/components/listOfUsers.jsx
@@ -1,4 +1,4 @@
-
+import Dummy from '../assets/Dummy.png'
 
 const ListOfUsers = (props) => {
 
@@ -15,7 +15,7 @@ const ListOfUsers = (props) => {
                 <div key={index} onClick={()=>{tagUser(user.name)}} className="flex gap-2 p-2 border-2 border-slate-700 m-2 text-slate-700 hover:bg-slate-700 hover:text-slate-300 cursor-pointer">
                     {user.name ?(
                     <>
-                        <img src={user.photo} alt="" className="w-[1.5em] rounded-[50%]"/>
+                        <img src={user.photo || Dummy} alt="" className="w-[1.5em] rounded-[50%]"/>
                         {user.name}
                     </> 
                     ):(
--- src/components/rightHome.jsx
+++ src/components/rightHome.jsx
@@ -1,18 +1,51 @@
-
+import { Link, useNavigate } from 'react-router-dom';
+import Dummy from '../assets/Dummy.png'
+import { useState } from 'react';
+import cat from '../axios'
 
 const RightHome = () => {
 
-    const handleSearchChange = () => {
+    const navigate = useNavigate();
+    const [searchName, setSearchName] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const apiurl = 'http://localhost:8000';
 
+    const handleSearchChange = (e) => {
+        setSearchName(e.target.value);
     }
 
-    const handleSearch = () => {
-        
+    const handleSearch = (e) => {
+        if(e.code=='Enter') {
+            cat.get(apiurl+'/user/whois/'+searchName+'/')
+            .then((resp) => {
+                setSearchResults(resp.data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            })
+        }
     }
 
     return (
-        <div className="w-[20%] h-[100%]">
-            <input className="rounded-[100px] p-2" type="text" name='search' id='search' placeholder='Search' onChange={handleSearchChange} onKeyDown={handleSearch} />
+        <div className="w-[20%] h-[100% p-2 items-center">
+            <input className="rounded-[100px] p-2 w-full" type="text" name='search' id='search' placeholder='Search' onChange={handleSearchChange} onKeyDown={handleSearch} />
+            <div className="w-full rounded">
+                {searchResults && searchResults.map((user, index) => (
+                    <div key={index} onClick={()=>{navigate("/profile/"+user.name)}} className="flex gap-2 p-2 border-2 border-slate-700 m-2 text-slate-700 hover:bg-slate-700 hover:text-slate-300 cursor-pointer">
+                        {user.name ?(
+                        <>
+                            <img src={user.photo || Dummy} alt="" className="w-[1.5em] rounded-[50%]"/>
+                            {user.name}
+                        </> 
+                        ):(
+                            <>
+                                {user.err}
+                            </> 
+                        )}
+                    </div>
+                ))}
+                {searchResults && <Link>search for posts</Link>}
+            </div>
         </div>
     )
 }
